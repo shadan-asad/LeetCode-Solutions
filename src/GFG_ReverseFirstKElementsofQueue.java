ebf90feb76172df6811835b01fd33a25499209c3
Shadan Asad <iamshadanasad@gmail.com>
2022-02-06 13:42:38 +0530

    GFG

---
+++ src/GFG_ReverseFirstKElementsofQueue.java
@@ -0,0 +1,27 @@
+import java.util.Queue;
+
+public class GFG_ReverseFirstKElementsofQueue {
+    class GfG {
+        // Function to reverse first k elements of a queue.
+        public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+            // add code here.
+            Object[] arr = q.toArray();
+            k--;
+            int i = 0;
+
+            while(i < k) {
+                Object temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+                i++;
+                k--;
+            }
+            q.clear();
+
+            for(Object x : arr) {
+                q.add((Integer)(x));
+            }
+            return q;
+        }
+    }
+}
