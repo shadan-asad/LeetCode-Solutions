64a787d3c9439c44fe7b78395ad2585d8296a3f9
shadan-asad <iamshadanasad@gmail.com>
2022-02-17 13:38:42 +0530

    GFG

--- src/_733_FloodFill.java
+++ src/_733_FloodFill.java
@@ -3,6 +3,9 @@ import java.util.ArrayDeque;
 public class _733_FloodFill {
     class Solution {
         public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+            if(image[sr][sc] == newColor)
+                return image;
+
             ArrayDeque<Integer[]> st = new ArrayDeque<>();
             int i,j;
             st.push(new Integer[]{sr,sc});
@@ -17,10 +20,6 @@ public class _733_FloodFill {
                 ifConnected(image, i,j-1, sr, sc, newColor, st);
                 ifConnected(image, i,j+1, sr, sc, newColor, st);
             }
-            for(int a=0; a < image.length; a++)
-                for(int b=0; b < image[0].length; b++)
-                    if(image[a][b] == -1)
-                        image[a][b] = newColor;
 
             image[sr][sc] = newColor;
             return image;
@@ -30,7 +29,7 @@ public class _733_FloodFill {
 
                 if(image[i][j] == image[sr][sc]) {
                     st.push(new Integer[]{i,j});
-                    image[i][j] = -1;
+                    image[i][j] = newColor;
                 }
             }
         }
