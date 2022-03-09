615f5d31377a15e89f45c0471c0ecb6a2f8872c7
shadan-asad <iamshadanasad@gmail.com>
2022-03-09 20:36:14 +0530

    LeetCode 1678

---
+++ src/_1678_GoalParserInterpretation.java
@@ -0,0 +1,20 @@
+public class _1678_GoalParserInterpretation {
+    class Solution {
+        public String interpret(String command) {
+            String str = "";
+            for(int i = 0; i < command.length(); i++) {
+                if(command.charAt(i) == 'G')
+                    str += 'G';
+                else if(command.charAt(i) == '(' && command.charAt(i+1) == ')') {
+                    str += 'o';
+                    i++;
+                }
+                else {
+                    str += "al";
+                    i += 3;
+                }
+            }
+            return str;
+        }
+    }
+}
