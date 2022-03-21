d344d029749ad0e7b2d46976ec76e187fe92716c
shadan-asad <iamshadanasad@gmail.com>
2022-03-21 10:18:33 +0530

    LeetCode 657

---
+++ src/_657_RobotReturntoOrigin.java
@@ -0,0 +1,24 @@
+public class _657_RobotReturntoOrigin {
+    class Solution {
+        public boolean judgeCircle(String moves) {
+            int x = 0, y = 0;
+            for(int i = 0; i < moves.length(); i++) {
+                if(moves.charAt(i) == 'U')
+                    x ++;
+                else if(moves.charAt(i) == 'D')
+                    x --;
+                else if(moves.charAt(i) == 'R')
+                    y++;
+                else
+                    y--;
+                System.out.println(x);
+            }
+            if(x == 0 && y == 0) {
+
+                return true;
+
+            }
+            return false;
+        }
+    }
+}
