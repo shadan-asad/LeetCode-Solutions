e4492ae1b97d07ed778ccdcbf5b9b23ea645d80a
shadan-asad <iamshadanasad@gmail.com>
2022-04-21 11:22:08 +0530

    LeetCode 700

---
+++ src/_700_SearchinaBinarySearchTree.java
@@ -0,0 +1,29 @@
+public class _700_SearchinaBinarySearchTree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            while(root != null && root.val != val) {
+                if(val < root.val) {
+                    root = root.left;
+                } else if(val > root.val){
+                    root = root.right;
+                }
+            }
+            return root;
+        }
+    }
+}
