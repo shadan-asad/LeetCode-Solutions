468e986d5bd2d5db0c707e631a4295a230724395
shadan-asad <iamshadanasad@gmail.com>
2022-04-23 10:31:47 +0530

    LeetCode 374

---
+++ src/_374_GuessNumberHigherorLower.java
@@ -0,0 +1,39 @@
+public class _374_GuessNumberHigherorLower {
+    /**
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is lower than the guess number
+     *			      1 if num is higher than the guess number
+     *               otherwise return 0
+     * int guess(int num);
+     */
+
+    public class Solution extends GuessGame {
+        public int guessNumber(int n) {
+            if(n == 1)
+                return n;
+
+
+
+            int start = 1, end = n+1, ans = 0;
+            while(start < end) {
+                int mid = start+(end-start)/2;
+
+                if(guess(mid) == 0) {
+                    return mid;
+                }
+
+                if(guess(mid) == 1) {
+                    start = mid+1;
+
+                }
+                else {
+                    end = mid;
+
+                }
+            }
+
+            return end-1;
+        }
+    }
+}
