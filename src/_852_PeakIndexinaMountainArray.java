238d8ccdc4d7d40290928a6101f760445b7eb6c5
shadan-asad <iamshadanasad@gmail.com>
2022-04-30 13:56:20 +0530

    LeetCode 852

---
+++ src/_852_PeakIndexinaMountainArray.java
@@ -0,0 +1,22 @@
+public class _852_PeakIndexinaMountainArray {
+    class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            int start = 0, end = arr.length-1;
+
+            while(start <= end) {
+                int mid = start + (end-start)/2;
+
+                if(arr[mid] > arr[mid+1]) {
+                    if(arr[mid] > arr[mid-1]) {
+                        return mid;
+                    } else {
+                        end = mid-1;
+                    }
+                }else {
+                    start = mid+1;
+                }
+            }
+            return -1;
+        }
+    }
+}
