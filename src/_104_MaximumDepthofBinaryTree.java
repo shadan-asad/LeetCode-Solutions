4d6926a00940926da444ddb5d37e3f59eb04267c
shadan-asad <iamshadanasad@gmail.com>
2022-05-10 23:38:57 +0530

    LeetCode 104

---
+++ src/_104_MaximumDepthofBinaryTree.java
@@ -0,0 +1,33 @@
+public class _104_MaximumDepthofBinaryTree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if(root == null)
+                return 0;
+            int h = 0;
+            int l = maxDepth(root.left);
+            int r = maxDepth(root.right);
+
+            if(l > r)
+                h = l+1;
+            else
+                h = r+1;
+
+            return h;
+        }
+    }
+}
