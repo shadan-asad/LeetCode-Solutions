6ff78bee4ead73ad6cd4bfd087af7eed34fe6fa5
shadan-asad <iamshadanasad@gmail.com>
2022-05-14 15:03:56 +0530

    LeetCode 414

---
+++ src/_414_ThirdMaximumNumber.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+public class _414_ThirdMaximumNumber {
+    import java.util.*;
+    class Solution {
+        public int thirdMax(int[] nums) {
+            HashSet<Integer> set = new HashSet<>();
+            for(int n : nums)
+                set.add(n);
+
+            int one = Integer.MIN_VALUE, two = Integer.MIN_VALUE, three = Integer.MIN_VALUE;
+
+            for(Integer i : set) {
+                if(i > one) {
+                    three = two;
+                    two = one;
+                    one = i;
+
+                }else if(i > two) {
+                    three = two;
+                    two = i;
+
+                }else if(i > three) {
+                    three = i;
+
+                }
+            }
+
+            if(set.size() < 3)
+                return one;
+
+            return three;
+        }
+    }
+}
