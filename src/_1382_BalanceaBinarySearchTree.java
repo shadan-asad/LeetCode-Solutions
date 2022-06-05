f20f5b426b5d22f36aeef7379497fb84fdbadfe7
shadan-asad <iamshadanasad@gmail.com>
2022-06-05 14:29:10 +0530

    LeetCode 1382

--- src/_1382_BalanceaBinarySearchTree.java
+++ src/_1382_BalanceaBinarySearchTree.java
@@ -1,52 +1,51 @@
-import java.util.ArrayList;
 import java.util.Stack;
 
-public class _1382_BalanceaBinarySearchTree {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
-    class Solution {
-        public TreeNode balanceBST(TreeNode root) {
-            ArrayList<Integer> list = new ArrayList<Integer>();
-            Stack<TreeNode> st = new Stack<TreeNode>();
-            while(root != null || !st.isEmpty()) {
-                while(root != null) {
-                    st.push(root);
-                    root = root.left;
-                }
-                root = st.pop();
-                list.add(root.val);
-                root = root.right;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        int[] arr = new int[10000];
+        int i = 0;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        while(root != null || !st.isEmpty()) {
+            while(root != null) {
+                st.push(root);
+                root = root.left;
             }
-
-            TreeNode node = balance(list, 0, list.size()-1);
-            return node;
+            root = st.pop();
+            arr[i] = root.val;
+            i++;
+            root = root.right;
         }
 
-        private TreeNode balance(ArrayList<Integer> list, int i, int j) {
-            if(i > j) {
-                return null;
-            }
-            int mid = i+(j-i)/2;
-            TreeNode node = new TreeNode(list.get(mid));
+        TreeNode node = balance(arr, 0, i-1);
+        return node;
+    }
 
-            node.left = balance(list, i, mid-1);
-            node.right = balance(list, mid+1, j);
-
-
-            return node;
+    private TreeNode balance(int[] list, int i, int j) {
+        if(i > j) {
+            return null;
         }
+        int mid = i+(j-i)/2;
+        TreeNode node = new TreeNode(list[mid]);
+
+        node.left = balance(list, i, mid-1);
+        node.right = balance(list, mid+1, j);
+
+
+        return node;
     }
 }
