bf69e9701466af7bf31f8fdbbae0093f3d77b090
shadan-asad <iamshadanasad@gmail.com>
2022-06-05 14:29:53 +0530

    LeetCode 1382

--- src/_1382_BalanceaBinarySearchTree.java
+++ src/_1382_BalanceaBinarySearchTree.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 /**
@@ -17,8 +18,7 @@ import java.util.Stack;
  */
 class Solution {
     public TreeNode balanceBST(TreeNode root) {
-        int[] arr = new int[10000];
-        int i = 0;
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
         Stack<TreeNode> st = new Stack<TreeNode>();
         while(root != null || !st.isEmpty()) {
             while(root != null) {
@@ -26,21 +26,20 @@ class Solution {
                 root = root.left;
             }
             root = st.pop();
-            arr[i] = root.val;
-            i++;
+            list.add(root);
             root = root.right;
         }
 
-        TreeNode node = balance(arr, 0, i-1);
+        TreeNode node = balance(list, 0, list.size()-1);
         return node;
     }
 
-    private TreeNode balance(int[] list, int i, int j) {
+    private TreeNode balance(ArrayList<TreeNode> list, int i, int j) {
         if(i > j) {
             return null;
         }
         int mid = i+(j-i)/2;
-        TreeNode node = new TreeNode(list[mid]);
+        TreeNode node = list.get(mid);
 
         node.left = balance(list, i, mid-1);
         node.right = balance(list, mid+1, j);
