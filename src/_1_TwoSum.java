b18be68a35a24ef314760699714e52c681be89d5
shadan-asad <iamshadanasad@gmail.com>
2022-06-19 08:51:56 +0530

    LeetCode 1

--- src/_1_TwoSum.java
+++ src/_1_TwoSum.java
@@ -1,17 +1,18 @@
+import java.util.HashMap;
+
 public class _1_TwoSum {
     class Solution {
         public int[] twoSum(int[] nums, int target) {
-            int arr[] = new int[2];
-            for(int i = 0; i < nums.length-1; i++) {
-                for(int j = i+1; j < nums.length; j++) {
-                    if(nums[i]+nums[j] == target) {
-                        arr[0] = i;
-                        arr[1] = j;
-                        return arr;
-                    }
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for(int i = 0; i < nums.length; i++) {
+                int temp = target-nums[i];
+                if(map.containsKey(temp)) {
+                    return new int[] {(int)(map.get(temp)), i};
                 }
+                map.put(nums[i], i);
             }
-            return arr;
+            return null;
         }
     }
 }
