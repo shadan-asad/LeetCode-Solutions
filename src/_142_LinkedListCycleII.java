a8d41c4d1e2436bc4ffc64ef1d9809a7090929ec
shadan-asad <iamshadanasad@gmail.com>
2022-06-27 14:47:55 +0530

    LeetCode 142

---
+++ src/_142_LinkedListCycleII.java
@@ -0,0 +1,38 @@
+public class _142_LinkedListCycleII {
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null)
+                return head;
+
+            ListNode slow = head, fast = head;
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast){
+                    do {
+                        if (head == slow.next) {
+                            return head;
+                        }
+                        slow = slow.next;
+                    } while (slow != fast);
+
+                    head = head.next;
+                }
+            }
+
+            return null;
+        }
+    }
+}
