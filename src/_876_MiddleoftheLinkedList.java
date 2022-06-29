789753b64c14b511faf659190d905fd467bc1019
shadan-asad <iamshadanasad@gmail.com>
2022-06-30 01:23:48 +0530

    LeetCode 876

--- src/_876_MiddleoftheLinkedList.java
+++ src/_876_MiddleoftheLinkedList.java
@@ -9,18 +9,26 @@ public class _876_MiddleoftheLinkedList {
      *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
     class Solution {
         public ListNode middleNode(ListNode head) {
             ListNode slow = head, fast = head;
 
-            while (fast.next != null && fast.next.next != null) {
+            while (fast != null && fast.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
             }
-            if (fast.next == null)
-                return slow;
 
-            return slow.next;
+            return slow;
         }
     }
 }
