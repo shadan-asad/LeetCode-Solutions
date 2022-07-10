27ee107303f79a4ed8cef33d7e606b6b98d70c98
shadan-asad <iamshadanasad@gmail.com>
2022-07-10 12:48:03 +0530

    LeetCode 496

--- src/_496_NextGreaterElementI.java
+++ src/_496_NextGreaterElementI.java
@@ -1,10 +1,10 @@
 public class _496_NextGreaterElementI {
     class Solution {
         public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
+            int j, temp;
             for(int i = 0; i < nums1.length; i++) {
-                int j = nums2.length-1;
-                int temp = -1;
+                j = nums2.length-1;
+                temp = -1;
                 while(nums1[i] != nums2[j]) {
                     if(nums2[j] > nums1[i]) {
                         temp = nums2[j];
