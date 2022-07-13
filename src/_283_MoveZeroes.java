19b30d73a79b85287f35322cde2fa690ef45f729
shadan-asad <iamshadanasad@gmail.com>
2022-07-14 02:33:00 +0530

    LeetCode 283

--- src/_283_MoveZeroes.java
+++ src/_283_MoveZeroes.java
@@ -1,18 +1,17 @@
+import java.util.ArrayList;
+
 public class _283_MoveZeroes {
     class Solution {
         public void moveZeroes(int[] nums) {
-            int count = 0;
-            for(int i = nums.length-1; i >= 0; i--) {
-                if(nums[i] == 0) {
-                    for(int j = i; j < nums.length-1-count; j++) {
-                        nums[j] = nums[j+1];
-                    }
-                    count++;
-                }
-            }
-            for(int i = nums.length-count; i < nums.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int x : nums)
+                if(x != 0)
+                    list.add(x);
+            int i = 0;
+            for(Integer x : list)
+                nums[i++] = x;
+            for(; i < nums.length; i++)
                 nums[i] = 0;
-            }
         }
     }
 }
