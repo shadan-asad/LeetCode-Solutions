a74e4042d4c8ff77d1495029b4374e4242c812c0
shadan-asad <iamshadanasad@gmail.com>
2022-07-15 14:59:39 +0530

    LeetCode 1480

---
+++ src/_1480_RunningSumof1dArray.java
@@ -0,0 +1,13 @@
+public class _1480_RunningSumof1dArray {
+    class Solution {
+        public int[] runningSum(int[] nums) {
+
+            int sum = 0;
+            for(int i = 0; i < nums.length; i++) {
+                nums[i] = sum+nums[i];
+                sum = nums[i];
+            }
+            return nums;
+        }
+    }
+}
