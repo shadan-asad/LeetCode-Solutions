6e87a32e217381a18f727180660f747630ac33ee
shadan-asad <iamshadanasad@gmail.com>
2022-07-16 02:28:26 +0530

    LeetCode 77

--- src/_77_Combinations.java
+++ src/_77_Combinations.java
@@ -1,34 +1,33 @@
 import java.util.*;
 public class _77_Combinations {
     class Solution {
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
 
         public List<List<Integer>> combine(int n, int k) {
-            int nums[] = new int[n];
+            List<Integer> nums = new ArrayList<>();
             for(int i = 1; i <= n; i++)
-                nums[i-1] = i;
-
-            for(List<Integer> x : helper(nums, 0)) {
-                if(x.size() == k)
-                    list.add(x);
-            }
-            return list;
+                nums.add(i);
+            return helper(nums, 0, k);
         }
 
-        private List<List<Integer>> helper(int[] nums, int i) {
-            if(i == nums.length) {
-                List<List<Integer>> base = new ArrayList<>();
-                base.add(new ArrayList<Integer>());
+        private List<List<Integer>> helper(List<Integer> nums, int i, int k) {
+            if(nums.size() - i == k) {
+                List<List<Integer>> base = new ArrayList<List<Integer>>();
+                base.add(nums.subList(i, nums.size()));
                 return base;
             }
 
-            List<List<Integer>> subAns = helper(nums, i+1);
+            List<List<Integer>> subAns = helper(nums, i+1, k);
+            HashSet<List<Integer>> set = new HashSet<List<Integer>>();
             int n = subAns.size();
             for(int j = 0; j < n; j++) {
-                List<Integer> temp = new ArrayList<Integer>(subAns.get(j));
-                temp.add(nums[i]);
-                subAns.add(temp);
+                for(int x = 0; x < subAns.get(j).size(); x++) {
+                    List<Integer> temp = new ArrayList<Integer>(subAns.get(j));
+                    temp.set(x, nums.get(i));
+                    Collections.sort(temp);
+                    set.add(temp);
+                }
             }
+            subAns.addAll(set);
             return subAns;
         }
     }
