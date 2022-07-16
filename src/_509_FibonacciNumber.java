787c8931baa98baa364f69bc35569321b47da2fa
shadan-asad <iamshadanasad@gmail.com>
2022-07-16 15:04:37 +0530

    LeetCode 509

---
+++ src/_509_FibonacciNumber.java
@@ -0,0 +1,17 @@
+public class _509_FibonacciNumber {
+    class Solution {
+        public int fib(int n) {
+            if(n == 1)
+                return 1;
+            if(n == 0)
+                return 0;
+            int a = 0, b = 1;
+            for(int i = 2; i <= n; i++) {
+                int temp = a+b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
+    }
+}
