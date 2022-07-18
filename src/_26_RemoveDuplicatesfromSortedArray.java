a5ea536849e51263f891d238c775e29f474538ee
shadan-asad <iamshadanasad@gmail.com>
2022-07-18 23:58:17 +0530

    LeetCode 26

--- src/_26_RemoveDuplicatesfromSortedArray.java
+++ src/_26_RemoveDuplicatesfromSortedArray.java
@@ -2,12 +2,12 @@ public class _26_RemoveDuplicatesfromSortedArray {
     class Solution {
         public int removeDuplicates(int[] nums) {
             int j = 0;
-            for(int i = 0; i < nums.length; i++) {
-                if(nums[i] != nums[j]) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != nums[j]) {
                     nums[++j] = nums[i];
                 }
             }
-            return j+1;
+            return j + 1;
         }
     }
 }
