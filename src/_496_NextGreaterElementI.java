bc9467879c945ebd691ba7e63e6c39b7593d3aeb
shadan-asad <iamshadanasad@gmail.com>
2022-08-21 02:01:06 +0530

    LeetCode 496

--- src/_496_NextGreaterElementI.java
+++ src/_496_NextGreaterElementI.java
@@ -1,19 +1,29 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 public class _496_NextGreaterElementI {
     class Solution {
         public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-            int j, temp;
-            for(int i = 0; i < nums1.length; i++) {
-                j = nums2.length-1;
-                temp = -1;
-                while(nums1[i] != nums2[j]) {
-                    if(nums2[j] > nums1[i]) {
-                        temp = nums2[j];
-                    }
-                    j--;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            Stack<Integer> st = new Stack<>();
+            int[] arr = new int[nums1.length];
+
+            for(int i = nums2.length-1; i > -1; i--) {
+                while(!st.isEmpty() && nums2[i] > st.peek()) {
+                    st.pop();
                 }
-                nums1[i] = temp;
+                if(st.isEmpty()) {
+                    map.put(nums2[i], -1);
+                }else {
+                    map.put(nums2[i], st.peek());
+                }
+                st.push(nums2[i]);
             }
-            return nums1;
+
+            for(int i = 0; i < nums1.length; i++) {
+                arr[i] = map.get(nums1[i]);
+            }
+            return arr;
         }
     }
 }
