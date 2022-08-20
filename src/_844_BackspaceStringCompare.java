4a2c0cd0b0987aeb749ec259756cac59e1df6a69
shadan-asad <iamshadanasad@gmail.com>
2022-08-21 02:18:51 +0530

    LeetCode 844

--- src/_844_BackspaceStringCompare.java
+++ src/_844_BackspaceStringCompare.java
@@ -1,45 +1,29 @@
+import java.util.Stack;
+
 public class _844_BackspaceStringCompare {
     class Solution {
         public boolean backspaceCompare(String s, String t) {
-            int i = 0, c, l = 0, j = -1;
-            while(i < s.length()) {
-                c = 0;
-                if(s.charAt(i) == '#') {
-                    while(i < s.length() && s.charAt(i) == '#') {
-                        c++; i++;
-                    }
-                    if(c <= l) {
-                        j = i-2*c;
-                    }else {
-                        j = i-(l+c);
-                    }
-                    if(j >= 0)
-                        s = s.substring(0, j) + s.substring(i, s.length());
-                    i = 0; l = 0;
-                } else {
-                    i++; l++;
+            Stack<Character> st = new Stack<>();
+            for(Character c : s.toCharArray()) {
+                if(c == '#') {
+                    if(!st.isEmpty())
+                        st.pop();
+                }else {
+                    st.push(c);
                 }
             }
+            s = st.toString();
+            st.clear();
 
-            i = 0; l = 0; j = -1;
-            while(i < t.length()) {
-                c = 0;
-                if(t.charAt(i) == '#') {
-                    while(i < t.length() && t.charAt(i) == '#') {
-                        c++; i++;
-                    }
-                    if(c <= l) {
-                        j = i-2*c;
-                    }else {
-                        j = i-(l+c);
-                    }
-                    if(j >= 0)
-                        t = t.substring(0, j) + t.substring(i, t.length());
-                    i = 0; l = 0;
+            for(Character c : t.toCharArray()) {
+                if(c == '#') {
+                    if(!st.isEmpty())
+                        st.pop();
                 } else {
-                    i++; l++;
+                    st.push(c);
                 }
             }
+            t = st.toString();
             if(s.equals(t))
                 return true;
             return false;
