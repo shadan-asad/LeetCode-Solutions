106f59bc4b75d16090167c14fb51ca6e5998830e
shadan-asad <iamshadanasad@gmail.com>
2022-08-30 00:49:18 +0530

    LeetCode 617

--- src/_617_MergeTwoBinaryTrees.java
+++ src/_617_MergeTwoBinaryTrees.java
@@ -1,43 +1,35 @@
 public class _617_MergeTwoBinaryTrees {
 
-     //* Definition for a binary tree node.
-      public class TreeNode {
-          int val;
-          TreeNode left;
-          TreeNode right;
-          TreeNode() {}
-          TreeNode(int val) { this.val = val; }
-          TreeNode(int val, TreeNode left, TreeNode right) {
-              this.val = val;
-              this.left = left;
-              this.right = right;
-          }
-      }
-
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
     class Solution {
         public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-
+            if(root1 == null && root2 == null) {
+                return null;
+            }
             if(root1 == null) {
                 return root2;
             }
             if(root2 == null) {
                 return root1;
             }
-
             root1.val += root2.val;
 
-            if(root1.left != null && root2.left != null) {
-                mergeTrees(root1.left, root2.left);
-            }
-            if(root1.right != null && root2.right != null) {
-                mergeTrees(root1.right, root2.right);
-            }
-            if(root1.left == null && root2.left != null) {
-                root1.left = root2.left;
-            }
-            if(root1.right == null && root2.right != null) {
-                root1.right = root2.right;
-            }
+            root1.left = mergeTrees(root1.left, root2.left);
+            root1.right = mergeTrees(root1.right, root2.right);
 
             return root1;
         }
