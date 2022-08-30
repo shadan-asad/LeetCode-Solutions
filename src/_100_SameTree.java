d17a23df6bd227ea14c39c7e11bb82de61d48db3
shadan-asad <iamshadanasad@gmail.com>
2022-08-31 00:36:53 +0530

    LeetCode 100

--- src/_100_SameTree.java
+++ src/_100_SameTree.java
@@ -18,48 +18,20 @@ public class _100_SameTree {
 
     class Solution {
         public boolean isSameTree(TreeNode p, TreeNode q) {
-            if(p == null && q == null)
+            if(p == null && q == null) {
                 return true;
-
-            if(p == null || q == null)
+            }
+            if(p == null || q == null) {
+                return false;
+            }
+            if(p.val != q.val)
                 return false;
 
-            ArrayDeque<TreeNode> st = new ArrayDeque<TreeNode>();
-            ArrayDeque<TreeNode> st2 = new ArrayDeque<TreeNode>();
+            boolean left = isSameTree(p.left, q.left);
+            boolean right = isSameTree(p.right, q.right);
 
-            while(p != null && q != null) {
-                if(p.val != q.val) {
-                    return false;
-                }
-
-                if(p.right != null) {
-                    st.push(p.right);
-                }
-                if(q.right != null) {
-                    st2.push(q.right);
-                }
-
-                if(st.size() != st2.size()) {
-                    return false;
-                }
-
-                p = p.left;
-                q = q.left;
-
-
-                if(p == null && (!st.isEmpty())) {
-                    p = st.pop();
-                }
-                if(q == null && (!st2.isEmpty())) {
-                    q = st2.pop();
-                }
-
-            }
-            if(st.isEmpty() && st2.isEmpty()) {
-                return true;
-            }
-
-            return false;
+            return (left && right);
         }
+    }
     }
 }
