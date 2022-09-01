9d5060bf02a2db632c2eb47b7da1ecc89bd566ed
shadan-asad <iamshadanasad@gmail.com>
2022-09-02 01:24:46 +0530

    LeetCode 101

--- src/_101_SymmetricTree.java
+++ src/_101_SymmetricTree.java
@@ -1,19 +1,4 @@
 public class _101_SymmetricTree {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
     class Solution {
         public boolean isSymmetric(TreeNode root) {
             return isSymm(root.left, root.right);
