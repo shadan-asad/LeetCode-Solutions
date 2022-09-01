ba614dcd53c615f377447a9dc46e2899921ad807
shadan-asad <iamshadanasad@gmail.com>
2022-09-02 01:01:04 +0530

    LeetCode 938

--- src/_938_RangeSumofBST.java
+++ src/_938_RangeSumofBST.java
@@ -2,41 +2,22 @@ import java.util.ArrayDeque;
 
 public class _938_RangeSumofBST {
 
-     //Definition for a binary tree node.
-      public class TreeNode {
-          int val;
-          TreeNode left;
-          TreeNode right;
-          TreeNode() {}
-          TreeNode(int val) { this.val = val; }
-          TreeNode(int val, TreeNode left, TreeNode right) {
-              this.val = val;
-              this.left = left;
-              this.right = right;
-          }
-      }
-
     class Solution {
+        int sum = 0;
         public int rangeSumBST(TreeNode root, int low, int high) {
-            int sum = 0;
-            ArrayDeque<TreeNode> st = new ArrayDeque<TreeNode>();
-
-            while(root != null) {
-                if(root.val >= low && root.val <= high) {
-                    sum += root.val;
-                    if(root.right != null) st.push(root.right);
-                    root = root.left;
-                }else {
-                    if(root.val < low) {
-                        root = root.right;
-                    }else if(root.val > high) {
-                        root = root.left;
-                    }
-                }
-                if(root == null && !st.isEmpty()) {
-                    root = st.pop();
-                }
+            if(root == null) return sum;
+            if(root.val < low) {
+                rangeSumBST(root.right, low, high);
             }
+            if(root.val > high) {
+                rangeSumBST(root.left, low, high);
+            }
+            if(root.val >= low && root.val <= high) {
+                sum += root.val;
+                rangeSumBST(root.left, low, high);
+                rangeSumBST(root.right, low, high);
+            }
+
             return sum;
         }
     }
