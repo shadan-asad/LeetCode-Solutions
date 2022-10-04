43cf5f2374ef7d4b3b539273598239097660d47d
shadan-asad <iamshadanasad@gmail.com>
2022-10-05 01:48:00 +0530

    LeetCode 771

--- src/_771_JewelsandStones.java
+++ src/_771_JewelsandStones.java
@@ -2,11 +2,9 @@ public class _771_JewelsandStones {
     class Solution {
         public int numJewelsInStones(String jewels, String stones) {
             int ans = 0;
-            for(int i = 0; i < jewels.length(); i++) {
-                for(int j = 0; j < stones.length(); j++) {
-                    if(jewels.charAt(i) == stones.charAt(j)) {
-                        ans++;
-                    }
+            for(int i = 0; i < stones.length(); i++) {
+                if(jewels.indexOf(stones.charAt(i)) != -1) {
+                    ans++;
                 }
             }
             return ans;
