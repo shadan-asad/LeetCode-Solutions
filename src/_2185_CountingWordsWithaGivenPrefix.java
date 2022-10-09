6e10eeae7ee3d16d469d6969a8fdfa5d5adb6181
shadan-asad <iamshadanasad@gmail.com>
2022-10-09 14:03:56 +0530

    LeetCode 2185

--- src/_2185_CountingWordsWithaGivenPrefix.java
+++ src/_2185_CountingWordsWithaGivenPrefix.java
@@ -1,3 +1,13 @@
 public class _2185_CountingWordsWithaGivenPrefix {
-
+    class Solution {
+        public int prefixCount(String[] words, String pref) {
+            int ans = 0;
+            for(String s : words) {
+                if(pref.length() <= s.length() && s.substring(0, pref.length()).equals(pref)) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
 }
