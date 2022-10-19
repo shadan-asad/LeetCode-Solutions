eb93b90279fe0fed87bb9c06ac6ff9bed8851b72
shadan-asad <iamshadanasad@gmail.com>
2022-10-19 20:53:01 +0530

    Codechef

--- src/Codechef_ODDSUMPAIR.java
+++ src/Codechef_ODDSUMPAIR.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
 
 public class Codechef_ODDSUMPAIR {
-    /* package codechef; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
     /* Name of the class has to be "Main" only if the class is public. */
     class Codechef
